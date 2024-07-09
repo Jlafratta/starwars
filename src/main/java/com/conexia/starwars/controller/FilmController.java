2ad7330f594f66f54681a2b634154d9bedf87c88
Julian <julianl@magnetico.dev>
2024-07-09 07:06:31 -0300

    add film resource

---
+++ src/main/java/com/conexia/starwars/controller/FilmController.java
@@ -0,0 +1,33 @@
+package com.conexia.starwars.controller;
+
+import com.conexia.starwars.domain.dto.FilmDTO;
+import com.conexia.starwars.domain.dto.PeopleDTO;
+import com.conexia.starwars.domain.dto.pagination.PageResult;
+import com.conexia.starwars.service.FilmService;
+import com.conexia.starwars.service.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/film")
+public class FilmController {
+
+    private final FilmService filmService;
+
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<PageResult<FilmDTO>> findAll(@RequestParam(required = false, defaultValue = "1") Integer page,
+                                                       @RequestParam(required = false, defaultValue = "10") Integer size,
+                                                       @RequestParam(required = false) Long id,
+                                                       @RequestParam(required = false) String title) {
+        return ResponseEntity.ok(filmService.findAll(page, size, id, title));
+    }
+}
